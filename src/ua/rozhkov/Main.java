b2e4bd6b1be8d684787a3f532dbba266125fdff3
PsyhoBelka <agroghkov@gmail.com>
2018-08-03 11:02:31 +0300

    Rewrite to OOP

--- src/ua/rozhkov/Main.java
+++ src/ua/rozhkov/Main.java
@@ -8,60 +8,27 @@ public class Main {
 
 		int minTicketNumber, maxTicketNumber;
 		int countSimple = 0, countHeavy = 0;
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Input min ticket number:");
-		minTicketNumber = Integer.parseInt(scanner.nextLine());
-		System.out.print("Input max ticket number: ");
-		maxTicketNumber = Integer.parseInt(scanner.nextLine());
 
-		if (minTicketNumber < maxTicketNumber) {
-			for (int i = minTicketNumber; i <= maxTicketNumber; i++) {
-				if (checkTicketSimple(i)) {
-					countSimple++;
-				}
-				if (checkTicketHeavy(i)) {
-					countHeavy++;
-				}
-			}
-			if (countSimple > countHeavy) {
-				System.out.println("Simple check win! " + countSimple + " vs " + countHeavy);
-			} else {
-				System.out.println("Heavy check win! " + countHeavy + " vs " + countSimple);
-			}
-		} else {
-			System.out.println("Wrong parametrs!");
+		try {
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Input min ticket number:");
+			minTicketNumber = Integer.parseInt(scanner.nextLine());
+			System.out.print("Input max ticket number: ");
+			maxTicketNumber = Integer.parseInt(scanner.nextLine());
+		}
+		catch (NumberFormatException e) {
+			System.out.println("Wrong ticket number!");
+			return;
 		}
 
-	}
+		Ticket ticket=new Ticket();
+		countSimple=ticket.countSimpleOnRange(minTicketNumber,maxTicketNumber);
+		countHeavy= ticket.countHeavyOnRange(minTicketNumber,maxTicketNumber);
 
-	static boolean checkTicketHeavy(int number) {
-
-		if ((number >= 0) && (number <= 999999)) {
-			int[] digits = new int[6];
-			int j = digits.length;
-			for (int i = String.valueOf(number).length() - 1; i >= 0; i--) {
-				digits[j - 1] = Integer.parseInt(String.valueOf(number).substring(i, i + 1));
-				j--;
-			}
-			if ((digits[0] + digits[2] + digits[4]) == (digits[1] + digits[3] + digits[5])) {
-				return true;
-			} else return false;
-		} else throw new NumberFormatException("Wrong ticket number!");
-	}
-
-	static boolean checkTicketSimple(int number) {
-
-		if ((number >= 0) && (number <= 999999)) {
-			int[] digits = new int[6];
-			int j = digits.length;
-			for (int i = String.valueOf(number).length() - 1; i >= 0; i--) {
-				digits[j - 1] = Integer.parseInt(String.valueOf(number).substring(i, i + 1));
-				j--;
-			}
-
-			if ((digits[0] + digits[1] + digits[2]) == (digits[3] + digits[4] + digits[5])) {
-				return true;
-			} else return false;
-		} else throw new NumberFormatException("Wrong ticket number!");
+		if (countSimple > countHeavy) {
+			System.out.println("Simple check win! " + countSimple + " vs " + countHeavy);
+		} else {
+			System.out.println("Heavy check win! " + countHeavy + " vs " + countSimple);
+		}
 	}
 }
